7e6c35b5c2daa166c815dff42ccbf49de5011233
Jose Lopez <gollodev@gmail.com>
2022-05-11 23:35:30 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import React from 'react'
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react'
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 import { 
     Box, 
     Input,
-    Button,
     Container,
-    Grid,
-    GridItem,
     HStack,
     VStack,
     Spinner,
